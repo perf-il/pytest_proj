329ff0cd309c4cef019613846c7225e613406824
Ilia Perfilov <ind.climb@gmail.com>
2023-03-01 16:21:28 +0300

    added fixture

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,5 +1,9 @@
+import pytest
 from utils import arrs
 
+@pytest.fixture
+def coll_1():
+    return {1: 'forest'}
 
 def test_get():
     assert arrs.get([1, 2, 3], 2, "test") == 3
@@ -13,9 +17,13 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3], -1) == [3]
     assert arrs.my_slice([1, 2, 3], -10) == [1, 2, 3]
 
-def test_get_val():
+
+def test_get_val(coll_1):
+
+    assert arrs.get_val(coll_1, 1) == 'forest'
+    assert arrs.get_val(coll_1, 2) == 'git'
+    assert arrs.get_val(coll_1, 2, 'cat') == 'cat'
+
+def test_get_val_empty():
     assert arrs.get_val({}, 'vsc') == 'git'
     assert arrs.get_val({}, 'vsc', 'cat') == 'cat'
-    assert arrs.get_val({1: 'forest'}, 1) == 'forest'
-    assert arrs.get_val({1: 'forest'}, 2) == 'git'
-    assert arrs.get_val({1: 'forest'}, 2, 'cat') == 'cat'
